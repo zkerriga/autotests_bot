a315ab5ab2b991498b6af82657083aa89fb044c4
Zkerriga <daniil.63@yandex.ru>
2020-04-24 12:51:26 +0300

    Fill the config template

--- config_template.py
+++ config_template.py
@@ -5,6 +5,13 @@
 #     By: zkerriga                                                 >^,^<     	  #
 #                                                                   / \     	  #
 #     Created: 2020-04-24 12:22:19 by zkerriga                     (___)__  	  #
-#     Updated: 2020-04-24 12:22:20 by zkerriga                              	  #
+#     Updated: 2020-04-24 12:50:46 by zkerriga                              	  #
 #                                                                             	  #
 # ******************************************************************************* #
+
+# Please refer to the website to generate these data. 
+# To do this, you will need to create an app.
+# https://my.telegram.org/
+
+api_id = 'your app id'
+api_hash = 'your app hash'
