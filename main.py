6d6ea9d6bc815dec8b7ff92f40f690b27424f11c
Zkerriga <daniil.63@yandex.ru>
2020-04-24 12:52:30 +0300

    First self message

--- main.py
+++ main.py
@@ -5,6 +5,12 @@
 #     By: zkerriga                                                 >^,^<     	  #
 #                                                                   / \     	  #
 #     Created: 2020-04-24 12:22:22 by zkerriga                     (___)__  	  #
-#     Updated: 2020-04-24 12:22:23 by zkerriga                              	  #
+#     Updated: 2020-04-24 12:45:45 by zkerriga                              	  #
 #                                                                             	  #
 # ******************************************************************************* #
+
+from pyrogram import Client
+from config import api_id, api_hash
+
+with Client("my_account", api_id, api_hash) as app:
+	app.send_message("me", "Братииииишка, я тебе покушать принёс!")
