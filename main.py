3439384a9889c07bd1dc8d4b71d429dead9f5ed7
Zkerriga <daniil.63@yandex.ru>
2020-04-24 21:35:00 +0300

    Update proccessing simple branch

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #     By: zkerriga                                                 >^,^<     	  #
 #                                                                   / \     	  #
 #     Created: 2020-04-24 12:22:22 by zkerriga                     (___)__  	  #
-#     Updated: 2020-04-24 20:28:09 by zkerriga                              	  #
+#     Updated: 2020-04-24 21:33:33 by zkerriga                              	  #
 #                                                                             	  #
 # ******************************************************************************* #
 
@@ -13,9 +13,15 @@ from pyrogram import Client, MessageHandler, Filters
 import time
 
 TEST_BOT = 1103314091
-status = 'begin' 
-# stack = []
-# cache = {}
+status = [
+			'begin', 'info_start', 'info_profilactic', 'info_profilactic_1',
+			'info_profilactic_1_ok', 'info_profilactic_2', 'info_profilactic_2_ok',
+			'info_profilactic_3', 'info_profilactic_3_ok', 'info_profilactic_end',
+			'info_start_2', 'info_start_lawyers', 'info_start_lawyers_1', 
+			'info_start_lawyers_1_ok', 'info_start_lawyers_2', 'info_start_lawyers_2_ok',
+			'info_start_lawyers_3', 'info_start_lawyers_3_ok', 'end'
+		 ]
+i = 0
 
 # api_id and api_hash located in the file config.ini
 app = Client("my_account")
@@ -25,107 +31,82 @@ app.send_message(TEST_BOT, "/start")
 @app.on_message(Filters.text)
 def begin(client, message):
 	global status
+	global i
 
-	time.sleep(1)
-	if status == 'begin':
+	if status[i] == 'begin':
 		print('[+] Bot started')
-		status = 'info_start'
+		i += 1
 		app.send_message(TEST_BOT, "Полезно знать")
-		time.sleep(1)
-	elif status == 'info_start':
+	elif status[i] == 'info_start':
 		print('[+] Info')
-		status = 'info_profilactic'
+		i += 1
 		app.send_message(TEST_BOT, "Напомнить меры профилактики")
-		time.sleep(1)
-	elif status == 'info_profilactic':
+	elif status[i] == 'info_profilactic':
 		print('[+] Info->Profilactic->1')
-		status = 'info_profilactic_1'
+		i += 1
 		app.send_message(TEST_BOT, "Как не заразиться?")
-		time.sleep(1)
-	elif status == 'info_profilactic_1':
+	elif status[i] == 'info_profilactic_1':
 		print('[+] Info->Profilactic->1->ok')
-		status = 'info_profilactic_1_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_profilactic_1_ok':
+	elif status[i] == 'info_profilactic_1_ok':
 		print('[+] Info->Profilactic->2')
-		status = 'info_profilactic_2'
+		i += 1
 		app.send_message(TEST_BOT, "Как не заразить окружающих?")
-		time.sleep(1)
-	elif status == 'info_profilactic_2':
+	elif status[i] == 'info_profilactic_2':
 		print('[+] Info->Profilactic->2->ok')
-		status = 'info_profilactic_2_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_profilactic_2_ok':
+	elif status[i] == 'info_profilactic_2_ok':
 		print('[+] Info->Profilactic->3')
-		status = 'info_profilactic_3'
+		i += 1
 		app.send_message(TEST_BOT, "Когда стоит вызывать врача?")
-		time.sleep(1)
-	elif status == 'info_profilactic_3':
+	elif status[i] == 'info_profilactic_3':
 		print('[+] Info->Profilactic->3->ok')
-		status = 'info_profilactic_3_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_profilactic_3_ok':
+	elif status[i] == 'info_profilactic_3_ok':
 		print('[+] Info->Profilactic->end')
-		status = 'info_profilactic_end'
+		i += 1
 		app.send_message(TEST_BOT, "Вернуться")
-		# time.sleep(1)
-	elif status == 'info_profilactic_end':
+	elif status[i] == 'info_profilactic_end':
 		print('[+] Info')
-		status = 'info_start_2'
+		i += 1
 		app.send_message(TEST_BOT, "Полезно знать")
-		time.sleep(1)
-	elif status == 'info_start_2':
+	elif status[i] == 'info_start_2':
 		print('[+] Info->Lawyers')
-		status = 'info_start_lawyers'
+		i += 1
 		app.send_message(TEST_BOT, "Советы юристов")
-		time.sleep(1)
-	elif status == 'info_start_lawyers':
+	elif status[i] == 'info_start_lawyers':
 		print('[+] Info->Lawyers->1')
-		status = 'info_start_lawyers_1'
+		i += 1
 		app.send_message(TEST_BOT, "Что будет, если нарушить карантин?")
-		time.sleep(1)
-
-	elif status == 'info_start_lawyers_1':
+	elif status[i] == 'info_start_lawyers_1':
 		print('[+] Info->Lawyers->1->ok')
-		status = 'info_start_lawyers_1_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_start_lawyers_1_ok':
+	elif status[i] == 'info_start_lawyers_1_ok':
 		print('[+] Info->Lawyers->2')
-		status = 'info_start_lawyers_2'
+		i += 1
 		app.send_message(TEST_BOT, "Как вести себя при задержании?")
-		time.sleep(1)
-	elif status == 'info_start_lawyers_2':
+	elif status[i] == 'info_start_lawyers_2':
 		print('[+] Info->Lawyers->2->ok')
-		status = 'info_start_lawyers_2_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_start_lawyers_2_ok':
+	elif status[i] == 'info_start_lawyers_2_ok':
 		print('[+] Info->Lawyers->3')
-		status = 'info_start_lawyers_3'
+		i += 1
 		app.send_message(TEST_BOT, "Могут ли меня уволить?")
-		time.sleep(1)
-	elif status == 'info_start_lawyers_3':
+	elif status[i] == 'info_start_lawyers_3':
 		print('[+] Info->Lawyers->3->ok')
-		status = 'info_start_lawyers_3_ok'
+		i += 1
 		app.send_message(TEST_BOT, "Понятно")
-		# time.sleep(1)
-	elif status == 'info_start_lawyers_3_ok':
+	elif status[i] == 'info_start_lawyers_3_ok':
 		print('[+] Info->Lawyers->end')
-		status = 'end'
+		i += 1
 		app.send_message(TEST_BOT, "Вернуться")
-		# time.sleep(1)
-	elif status == 'end':
+	elif status[i] == 'end':
 		print("[+] SUCCESS")
-	else:
-		print('[-] Error! Stopped status:', status)
-
-
-
-
 
 # @app.on_message()
 # def main_answer(client, message):
