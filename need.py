d461c27ce7156f780a85ad74bb2cc465532f5cc6
Zkerriga <daniil.63@yandex.ru>
2020-04-25 23:31:50 +0300

    Add basic structure

--- need.py
+++ need.py
@@ -5,7 +5,7 @@
 #     By: zkerriga                                                 >^,^<     	  #
 #                                                                   / \     	  #
 #     Created: 2020-04-25 22:20:56 by zkerriga                     (___)__  	  #
-#     Updated: 2020-04-25 22:20:57 by zkerriga                              	  #
+#     Updated: 2020-04-25 23:28:52 by zkerriga                              	  #
 #                                                                             	  #
 # ******************************************************************************* #
 
@@ -19,13 +19,14 @@ TEST_BOT = 1103314091
 BACK = 'Вернуться'
 GOTIT = 'Понятно'
 flag = False
-want_to_help = [ BACK, 'Поддержать НКО и местные инициативы',
-				'Распространить полезную информацию',
-				'Стать волонтером', 'Обратная связь']
-become_a_volunteer = [BACK, 'У меня есть медицинские навыки',
-					 'Хочу помочь нуждающимся']
-who_need_help = [BACK, 'Уязвимым людям', 'Медицинским работникам',
-				'Малому бизнесу и культуре']
+need_help = [ BACK, 'Мои права нарушают', 'Другая помощь']
+another_help = [BACK, 'Связаться с волонтерами', 'Психологическая помощь',
+				'Помощь врачам', 'Продукты питания']
+rights_troubles = [BACK, 'Задержали полицейские', 'Принудительно госпитализируют',
+					'Проблемы в университете', 'Проблемы с работой']
+job_troubles = [BACK, 'Сократили на работе', 'Работодатель подвергает риску']
+hospitalize = [BACK, 'Я против госпитализации. Что делать?',
+				'Мне угрожают уголовной ответственностью. Это законно?']
 
 client = TelegramClient('zkerriga', api_id, api_hash)
 
@@ -33,81 +34,46 @@ client = TelegramClient('zkerriga', api_id, api_hash)
 async def handler(event):
 	logging.info('[+] Bot started')
 	print('[+] Bot started')
-	await client.send_message(TEST_BOT, 'Хочу помочь')
+	await client.send_message(TEST_BOT, 'Нужна помощь')
 
-@client.on(events.NewMessage(pattern=r'Отлично! Вот несколько вариантов\.'))
+@client.on(events.NewMessage(pattern=r'Что с вами произошло.*'))
 async def handler(event):
-	logging.info('[+] Want to help started')
-	print('[+] Want to help started')
-	await client.send_message(TEST_BOT, want_to_help.pop())
+	logging.info('[+] Need help started')
+	print('[+] Need help started')
+	await client.send_message(TEST_BOT, need_help.pop())
 
-@client.on(events.NewMessage(pattern=r'Напишите здесь, что вы хотели бы мне передать\.'))
+@client.on(events.NewMessage(pattern=r'Как еще вам можно помочь.*'))
 async def handler(event):
-	logging.info('[+] Want to help -> Feedback')
-	print('[+] Want to help -> Feedback')
-	await client.send_message(TEST_BOT, "[+] Zkerriga's test feedback")
+	logging.info('[+] Need help -> Another')
+	print('[+] Need help -> Another')
+	await client.send_message(TEST_BOT, another_help.pop())
 
-@client.on(events.NewMessage(pattern=r'Спасибо за ваше сообщение, оно передано разработчикам!.*'))
+@client.on(events.NewMessage(pattern=r'.*которая занимается помощью мигрантам.*'))
 async def handler(event):
-	logging.info('[+] Want to help -> Feedback -> Send')
-	print('[+] Want to help -> Feedback -> Send')
-	await client.send_message(TEST_BOT, BACK)
-
-@client.on(events.NewMessage(pattern=r'.*test feedback'))
-async def handler(event):
-	if event.forward and event.text.find('test feedback') > 0:
-		logging.info('[+] Want to help -> Feedback -> Send -> Ok')
-		print('[+] Want to help -> Feedback -> Send -> Ok')
-
-@client.on(events.NewMessage(pattern=r'Какая форма волонтерства вам подходит.*'))
-async def handler(event):
-	logging.info('[+] Want to help -> Voluntier')
-	print('[+] Want to help -> Voluntier')
-	await client.send_message(TEST_BOT, become_a_volunteer.pop())
-
-@client.on(events.NewMessage(pattern=r'.*волонтеры фонда помогают.*'))
-async def handler(event):
-	logging.info('[+] Want to help -> Voluntier -> I want -> Ok')
-	print('[+] Want to help -> Voluntier -> I want -> Ok')
+	logging.info('[+] Need help -> Another -> Food -> Ok')
+	print('[+] Need help -> Another -> Food -> Ok')
 	await client.send_message(TEST_BOT, GOTIT)
 
-@client.on(events.NewMessage(pattern=r'.*главной больницы Москвы.*'))
+@client.on(events.NewMessage(pattern=r'.*благотворительный фонд «Правмир».*'))
 async def handler(event):
-	logging.info('[+] Want to help -> Voluntier -> I have -> Ok')
-	print('[+] Want to help -> Voluntier -> I have -> Ok')
+	logging.info('[+] Need help -> Another -> Doctors -> Ok')
+	print('[+] Need help -> Another -> Doctors -> Ok')
 	await client.send_message(TEST_BOT, GOTIT)
 
-@client.on(events.NewMessage(pattern=r'.*распространить информацию о коронавирусе.*'))
+@client.on(events.NewMessage(pattern=r'.*в онлайн-чате или по горячей линии .*'))
 async def handler(event):
-	logging.info('[+] Want to help -> Share -> Ok')
-	print('[+] Want to help -> Share -> Ok')
+	logging.info('[+] Need help -> Another -> Pcihol -> Ok')
+	print('[+] Need help -> Another -> Pcihol -> Ok')
 	await client.send_message(TEST_BOT, GOTIT)
 
-@client.on(events.NewMessage(pattern=r'Кому вы хотели бы помочь.*'))
+@client.on(events.NewMessage(pattern=r'Пришлите вашу геолокацию.*'))
 async def handler(event):
-	logging.info('[+] Want to help -> Who need help')
-	print('[+] Want to help -> Who need help')
-	await client.send_message(TEST_BOT, who_need_help.pop())
-
-@client.on(events.NewMessage(pattern=r'.*местных предпринимателей рядом с вами.*'))
-async def handler(event):
-	logging.info('[+] Want to help -> Who need help -> Business -> Ok')
-	print('[+] Want to help -> Who need help -> Business -> Ok')
-	await client.send_message(TEST_BOT, GOTIT)
-
-@client.on(events.NewMessage(pattern=r'.*акцию помощи одиноким пожилым людям.*'))
-async def handler(event):
-	logging.info('[+] Want to help -> Who need help -> Grandpeople -> Ok')
-	print('[+] Want to help -> Who need help -> Grandpeople -> Ok')
-	await client.send_message(TEST_BOT, GOTIT)
-
-@client.on(events.NewMessage(pattern=r'.*сбор средств на покупку индивидуальной защиты.*'))
-async def handler(event):
-	logging.info('[+] Want to help -> Who need help -> Medecine -> Ok')
-	print('[+] Want to help -> Who need help -> Medecine -> Ok')
-	global flag
-	flag = True
-	await client.send_message(TEST_BOT, GOTIT)
+	logging.info('[+] Need help -> Another -> Geo')
+	logging.info('[-] Need help -> Another -> Geo <- Not sended')
+	print('[+] Need help -> Another -> Geo')
+	print('[-] Need help -> Another -> Geo <- Not sended')
+	await client.send_message(TEST_BOT, geo_point=(30.379251039815593, 59.80547555591638))
+	await client.send_message(TEST_BOT, 'Отмена')
 
 @client.on(events.NewMessage(pattern=r'Чем еще я могу быть полезен.*'))
 async def handler(event):
@@ -115,6 +81,70 @@ async def handler(event):
 		logging.info('[+] SUCCESS')
 		print('[+] SUCCESS\n')
 		exit(0)
+	else:
+		await client.send_message(TEST_BOT, 'Нужна помощь')
+
+@client.on(events.NewMessage(pattern=r'Какая у вас проблема.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights')
+	print('[+] Need help -> Rights')
+	await client.send_message(TEST_BOT, rights_troubles.pop())
+
+@client.on(events.NewMessage(pattern=r'Какая именно у вас проблема.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights -> Job')
+	print('[+] Need help -> Rights -> Job')
+	await client.send_message(TEST_BOT, job_troubles.pop())
+
+@client.on(events.NewMessage(pattern=r'.*Работодатель требует выйти на работу.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights -> Job -> Risk -> Ok')
+	print('[+] Need help -> Rights -> Job -> Risk -> Ok')
+	await client.send_message(TEST_BOT, GOTIT)
+
+@client.on(events.NewMessage(pattern=r'.*Что делать, если работодатель решил.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights -> Job -> Fired -> Ok')
+	print('[+] Need help -> Rights -> Job -> Fired -> Ok')
+	await client.send_message(TEST_BOT, GOTIT)
+
+@client.on(events.NewMessage(pattern=r'Нужна ли вам еще юридическая помощь.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights')
+	print('[+] Need help -> Rights')
+	await client.send_message(TEST_BOT, rights_troubles.pop())
+
+@client.on(events.NewMessage(pattern=r'.*Студенческий журнал DOXA.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Rights -> University -> Ok')
+	print('[+] Need help -> Rights -> University -> Ok')
+	await client.send_message(TEST_BOT, GOTIT)
+
+@client.on(events.NewMessage(pattern=r'Что именно происходит.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Hospitalize')
+	print('[+] Need help -> Hospitalize')
+	await client.send_message(TEST_BOT, hospitalize.pop())
+
+@client.on(events.NewMessage(pattern=r'.*На текущий момент уголовная ответственность.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Hospitalize -> Menaces-> Ok')
+	print('[+] Need help -> Hospitalize -> Menaces -> Ok')
+	await client.send_message(TEST_BOT, GOTIT)
+
+@client.on(events.NewMessage(pattern=r'.*Если гражданин категорически не согласен.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Hospitalize -> Denie-> Ok')
+	print('[+] Need help -> Hospitalize -> Denie -> Ok')
+	await client.send_message(TEST_BOT, GOTIT)
+
+@client.on(events.NewMessage(pattern=r'.*К сожалению, введение режима повышенной.*'))
+async def handler(event):
+	logging.info('[+] Need help -> Hospitalize -> Arrest -> Ok')
+	print('[+] Need help -> Hospitalize -> Arrest -> Ok')
+	global flag
+	flag = True
+	await client.send_message(TEST_BOT, GOTIT)
 
 client.start()
 client.run_until_disconnected()
